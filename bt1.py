d247714a96ddc18676c595501926bf5786ea77db
25112081 <minh39vn@gmail.com>
2026-03-26 01:09:20 +0700

    Delete bt1.py

--- bt1.py
+++
@@ -1,9 +0,0 @@
-class HangHoa:
-    def __init__(self, ma_hang, ten_hang, nha_san_xuat, gia):
-        self.ma_hang = ma_hang
-        self.ten_hang = ten_hang
-        self.nha_san_xuat = nha_san_xuat
-        self.gia = gia
-    
-    def get_ma_hang(self):
-        return self.ma_hang
